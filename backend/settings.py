a0dcd581f551d5e9e13114e54aa88f6eb2089e16
Ismam Khan <ismamkhan@Ismams-MacBook-Pro.local>
2024-02-14 03:12:37 +0600

    i

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-93ck8@b+x!twvhsk$ono$f6&u!=o#_-_wwpmqlv^&f*8pa0_1%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
